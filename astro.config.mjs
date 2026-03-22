539cbddffd776d509e04294166f0d3e8cafbeb43
Riverxia <riverdalecoder@outlook.com>
2026-03-22 19:18:31 -0400

    (acm) Currently Listening To:  -

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'idkthedocs',
 			components: {
 				PageTitle: './src/components/PageTitle.astro',
 			},
